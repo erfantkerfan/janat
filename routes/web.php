16626fda135d789a492e4294188e004ffbbc5c21
kerasus <aliesmaeeli20@gmail.com>
2020-12-12 13:24:36 +0330

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+use App\Http\Controllers\FundController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\UserStatusController;
 
-Route::get('/home', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'welcome'])->name('web.welcome');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('web.panel.dashboard');
+    Route::group(['prefix' => 'api'], function () {
+        Route::resource('users', '\\'.UserController::class);
+        Route::group(['prefix' => 'users'], function () {
+            Route::get('{user}/user_pic', [UserController::class, 'getUserPic'])->name('web.panel.getUserPic');
+            Route::put('{user}/reset_pass', [UserController::class, 'resetPass'])->name('web.panel.getUserPic');
+        });
+        Route::resource('user_statuses', '\\'. UserStatusController::class);
+        Route::resource('companies', '\\'. CompanyController::class);
+        Route::resource('funds', '\\'. FundController::class);
+        Route::resource('accounts', '\\'. AccountController::class);
+    });
+});
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
