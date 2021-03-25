99c499b56303b1a48db563ab64e4a583ed19747b
Ali <aliesmaeeli20@gmail.com>
2021-03-26 00:29:02 +0430

    update validation fa lang

--- resources/lang/fa/validation.php
+++ resources/lang/fa/validation.php
@@ -87,7 +87,7 @@ return [
     'min' => [
         'numeric' => 'The :attribute must be at least :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
+        'string' => 'مقدار :attribute می بایست حداقل :min کاراکتر باشد.',
         'array' => 'The :attribute must have at least :min items.',
     ],
     'not_in' => 'The selected :attribute is invalid.',
