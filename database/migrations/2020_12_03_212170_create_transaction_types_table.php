d50bb2c407e3cda8c74ce880c3058f18ea802385
Ali <aliesmaeeli20@gmail.com>
2021-03-28 12:06:04 +0430

    update database for transaction types

---
+++ database/migrations/2020_12_03_212170_create_transaction_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_types', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name')->nullable()->comment('نام');
+            $table->string('display_name')->nullable()->comment('نام قابل نمایش');
+            $table->longText('description')->nullable()->comment('توضیح نوع تراکنش');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_types');
+    }
+}
